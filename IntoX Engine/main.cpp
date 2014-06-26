f6de9664aa4a5be824c0fb7f526405647c414a18
DaeFennek <DaeFennek@gmail.com>
2014-06-26 22:44:06 +0200

    Init Project

---
+++ IntoX Engine/main.cpp
@@ -0,0 +1,10 @@
+#include "stdfax.h"
+
+
+int main(int argv, char** argc)
+{
+	std::cout << "** IntoX Engine **" << std::endl;
+	system("pause");
+	return 0;
+
+}
---
+++ IntoX Engine/stdfax.h
@@ -0,0 +1,7 @@
+#ifndef __STDFAX_H__
+#define __STDFAX_H__
+
+#include <iostream>
+
+
+#endif //__STDFAX_H__
